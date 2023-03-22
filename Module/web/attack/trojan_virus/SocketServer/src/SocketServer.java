55c307c201168afaa2520f0eaf3d2e62a2d2cc75
LinWin-Cloud <2382546858@qq.com>
2023-03-22 20:29:50 +0800

    Module/

--- Module/web/attack/trojan_virus/SocketServer/src/SocketServer.java
+++ Module/web/attack/trojan_virus/SocketServer/src/SocketServer.java
@@ -1,2 +1,58 @@
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class SocketServer {
+    public static boolean connect = false;
+    public static void main(String[] args) {
+        Thread options = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
+                String command = scanner.nextLine();
+
+            }
+        });
+        options.start();
+
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(100);
+            int port = Integer.parseInt(args[0]);
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Future<Integer> future = executorService.submit(new Callable<Integer>() {
+                    @Override
+                    public Integer call() throws Exception {
+                        SocketServer.connect = true;
+                        OutputStream outputStream = socket.getOutputStream();
+                        InputStream inputStream = socket.getInputStream();
+
+                        PrintWriter printWriter = new PrintWriter(outputStream);
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                        while (true) {
+                            String message = bufferedReader.readLine();
+                            if (message == null) {
+                                break;
+                            }
+                            System.out.println(message);
+                        }
+                        bufferedReader.close();
+
+                        return 0;
+                    }
+                });
+            }
+        }
+        catch (Exception exception) {
+            System.out.println("ERROR");
+        }
+    }
 }
