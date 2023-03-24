3439474754f6345b938c20bae957f16afcac7985
LinWin-Cloud <2382546858@qq.com>
2023-03-24 19:19:42 +0800

    MainTerminal/

--- MainTerminal/ConfigConsole.py
+++ MainTerminal/ConfigConsole.py
@@ -1,7 +1,24 @@
+import main as main
+
 
 def configConsole():
     print("""
  1. Java Runtime Setting                    Enter '1'
  2. Version                                 Enter '2'
- 3. 
+ 3. About                                   Enter '3'
+ 4. exit                                    Enter '4'
     """)
+    while True:
+        options = input("LinwinSploit-"+main.version+" (Config) $ ")
+        options = options.strip()
+
+        if options == '1':
+            pass
+        if options == '2':
+            pass
+        if options == '3':
+            pass
+        if options == '4':
+            break
+    
+    main.main()
